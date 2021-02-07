1459ee712987b85f36278cf9e69f9b79f5a8293d
Rendy Artha <rendyartha312@gmail.com>
2021-02-07 21:04:33 +0700

    tugas kelompok done

---
+++ js/catalog.js
@@ -0,0 +1,49 @@
+import {dataCatalog} from "./data-util/data.js";
+
+// console.log(dataCatalog);
+
+
+if(localStorage.categoryItem == null){
+  localStorage.categoryItem = "all";
+}
+// console.log(localStorage.categoryItem);
+// delete localStorage.categoryItem;
+
+
+let catalogID = document.getElementById('catalogID');
+let catalogTitle = document.getElementById('catalogTitle');
+
+let display = ()=>{
+
+    if(localStorage.categoryItem == 'kaos-polo'){
+      catalogTitle.innerText = "KAOS POLO";
+    } else if(localStorage.categoryItem == 'kaos-polos'){
+      catalogTitle.innerText = "KAOS POLOS";
+    } else if(localStorage.categoryItem == 'kemeja'){
+      catalogTitle.innerText = "KEMEJA";
+    } else{
+      catalogTitle.innerText = "ALL CATEGORY";
+    }
+
+    dataCatalog.forEach(item =>{
+
+      if(localStorage.categoryItem == item.categoryItem || localStorage.categoryItem == "all"){     
+        let card = document.createElement("div");
+        card.setAttribute("class","col-md-6 col-lg-4 my-5 justify-content-center d-flex");
+        card.innerHTML = `
+            <div class="card shadow border-primary my-card">
+                <img src="${item.image}" class="card-img-top img-fluid"  alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${item.nama}</h5>
+                  <p class="card-text my-2">Rp ${item.price}</p>
+                  <a onclick="goToCatalogDetail(${item.id})" href="catalog-detail.html" class="btn btn-main w-100 ">Detail item</a>
+                </div>
+            </div>
+        `;
+        catalogID.appendChild(card);
+      }
+
+    });
+}
+
+display();
